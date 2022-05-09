f2a8f5f4de546cfaab9d1ba07268c5c0ea275225
xiaoyang-chen <xiaoyang-chen@xxx.com>
2022-05-09 15:19:03 +0800

    feat: 修正日志显示的调用函数名称都是包装函数的问题

--- log/logger.go
+++ log/logger.go
@@ -6,6 +6,10 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// "debug","info","warn","error","dPanic","panic","fatal", default "info"
+// these value can't not be Change
+var Debug, Info, Warn, Error, DPanic, Panic, Fatal func(msg string, fields ...field)
+
 var _logger *zap.Logger // consider using atomic.Value for data race
 
 func Init(serviceName, loglevel string, lumberjackLogger lumberjack.Logger) {
@@ -50,21 +54,31 @@ func Init(serviceName, loglevel string, lumberjackLogger lumberjack.Logger) {
 	var filed = zap.Fields(zap.String("service_name", serviceName))
 	// 构造日志
 	_logger = zap.New(core, caller, filed)
+	// 设置日志函数指针
+	{
+		Debug = getLogger().Debug
+		Info = getLogger().Info
+		Warn = getLogger().Warn
+		Error = getLogger().Error
+		DPanic = getLogger().DPanic
+		Panic = getLogger().Panic
+		Fatal = getLogger().Fatal
+	}
 	Info("logger init success")
 	// var testErr = func() error { return errors.New("testErr") } // errors == "github.com/pkg/errors"
 	// Info("test", String("string", "aaa"), Int("int", 1), Error2Field(testErr()))
-	// log file will show as follow
+	// log file will show like below
 	// {"level":"INFO","time":"2022-05-09T09:08:50.867+0800","line_num":"xxx/logger.go:73","func":"xxx/logger.Info","msg":"test","service_name":"xxx","string":"aaa","int":1,"error":"testErr","errorVerbose":"testErr\nxxx/chenxiaoyang/xxx.func1\n\t/xxx/logger.go:61\n/xxx/logger.Init\n\t/xxx/logger.go:62\nmain.main.func1\n\t/xxx/main.go:14\nruntime.goexit\n\t/xxx/go/1.18.1/libexec/src/runtime/asm_amd64.s:1571"}
 }
 
 // func getLogger() *zap.Logger { return _logger.Load().(*zap.Logger) }
 func getLogger() *zap.Logger { return _logger }
 
-// "debug","info","warn","error","dPanic","panic","fatal", default "info"
-func Debug(msg string, fields ...field)  { getLogger().Debug(msg, fields...) }
-func Info(msg string, fields ...field)   { getLogger().Info(msg, fields...) }
-func Warn(msg string, fields ...field)   { getLogger().Warn(msg, fields...) }
-func Error(msg string, fields ...field)  { getLogger().Error(msg, fields...) }
-func DPanic(msg string, fields ...field) { getLogger().DPanic(msg, fields...) }
-func Panic(msg string, fields ...field)  { getLogger().Panic(msg, fields...) }
-func Fatal(msg string, fields ...field)  { getLogger().Fatal(msg, fields...) }
+// 包装函数会导致打印出来的调用函数显示为以下包装函数的名称
+// func Debug(msg string, fields ...field) { getLogger().Debug(msg, fields...) }
+// func Info(msg string, fields ...field)   { getLogger().Info(msg, fields...) }
+// func Warn(msg string, fields ...field)   { getLogger().Warn(msg, fields...) }
+// func Error(msg string, fields ...field)  { getLogger().Error(msg, fields...) }
+// func DPanic(msg string, fields ...field) { getLogger().DPanic(msg, fields...) }
+// func Panic(msg string, fields ...field)  { getLogger().Panic(msg, fields...) }
+// func Fatal(msg string, fields ...field)  { getLogger().Fatal(msg, fields...) }
